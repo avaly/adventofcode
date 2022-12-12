649eeb81467fcd187f90d611868a2742462d4dda
Valentin Agachi <github-com@agachi.name>
2022-12-12 18:50:38 +0100

    Day 12 - faster

--- 12/12.go
+++ 12/12.go
@@ -31,45 +31,46 @@ func Run(input string) {
 		heightmap = append(heightmap, heights)
 	}
 
-	PrintMap(heightmap)
+	// PrintMap(heightmap)
 
-	part1 := 0
+	stepsUp := ShortestPath(heightmap, start, func (current int, next int) bool {
+		return next <= current + 1;
+	})
+	stepsDown := ShortestPath(heightmap, finish, func (current int, next int) bool {
+		return next >= current - 1;
+	})
+
+	part1 := stepsUp[finish[0]][finish[1]] - 1
 	part2 := len(heightmap) * len(heightmap[0]) + 1
 
 	for y := 0; y < len(heightmap); y++ {
 		for x := 0; x < len(heightmap[0]); x++ {
-			if (heightmap[y][x] == 1) {
-				steps := ShortestPath(heightmap, [2]int{y, x}, finish)
-				if (start[0] == y && start[1] == x) {
-					part1 = steps
-				}
-				if (steps > 0 && steps < part2) {
-					fmt.Println(y, x, steps)
-					part2 = steps
-				}
+			if (heightmap[y][x] == 1 && stepsDown[y][x] > 0 && stepsDown[y][x] < part2) {
+				part2 = stepsDown[y][x]
 			}
 		}
 	}
+	part2--;
 
-	fmt.Printf("Part 1: %v\n\n", part1)
+	fmt.Printf("Part 1: %v\n", part1)
 	fmt.Printf("Part 2: %v\n", part2)
 }
 
-func ShortestPath(heightmap [][]int, start [2]int, finish [2]int) int {
+func ShortestPath(heightmap [][]int, start [2]int, canVisit func (int, int) bool) [][]int {
 	steps := make([][]int, 0)
 
 	for y := 0; y < len(heightmap); y++ {
 		steps = append(steps, make([]int, len(heightmap[0])))
 	}
 
-	VisitUp(heightmap, steps, start[0], start[1], 1)
+	Visit(heightmap, steps, start[0], start[1], 1, canVisit)
 
 	// PrintMap(steps)
 
-	return steps[finish[0]][finish[1]] - 1
+	return steps
 }
 
-func VisitUp(heightmap [][]int, steps [][]int, y int, x int, count int) {
+func Visit(heightmap [][]int, steps [][]int, y int, x int, count int, canVisit func (int, int) bool) {
 	if (steps[y][x] > 0 && steps[y][x] <= count) {
 		return
 	}
@@ -78,17 +79,17 @@ func VisitUp(heightmap [][]int, steps [][]int, y int, x int, count int) {
 
 	current := heightmap[y][x]
 
-	if (y > 0 && heightmap[y - 1][x] <= current + 1) {
-		VisitUp(heightmap, steps, y - 1, x, count + 1)
+	if (y > 0 && canVisit(current, heightmap[y - 1][x])) {
+		Visit(heightmap, steps, y - 1, x, count + 1, canVisit)
 	}
-	if (y < len(heightmap) - 1 && heightmap[y + 1][x] <= current + 1) {
-		VisitUp(heightmap, steps, y + 1, x, count + 1)
+	if (y < len(heightmap) - 1 && canVisit(current, heightmap[y + 1][x])) {
+		Visit(heightmap, steps, y + 1, x, count + 1, canVisit)
 	}
-	if (x > 0 && heightmap[y][x - 1] <= current + 1) {
-		VisitUp(heightmap, steps, y, x - 1, count + 1)
+	if (x > 0 && canVisit(current, heightmap[y][x - 1])) {
+		Visit(heightmap, steps, y, x - 1, count + 1, canVisit)
 	}
-	if (x < len(heightmap[0]) - 1 && heightmap[y][x + 1] <= current + 1) {
-		VisitUp(heightmap, steps, y, x + 1, count + 1)
+	if (x < len(heightmap[0]) - 1 && canVisit(current, heightmap[y][x + 1])) {
+		Visit(heightmap, steps, y, x + 1, count + 1, canVisit)
 	}
 }
 
