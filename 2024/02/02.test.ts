6be6b07f446591df1d376f08fd5265ef5fe4ff2d
Valentin Agachi <github-com@agachi.name>
2024-12-02 09:25:13 +0100

    Day 02

--- 2024/02/02.test.ts
+++ 2024/02/02.test.ts
@@ -2,23 +2,33 @@ import { strictEqual } from 'node:assert';
 
 import { cases, sample, testsFor } from '../../utils/tests';
 
-testsFor(['2024', '02'], (day, { part1, part2 }) => {
+testsFor(['2024', '02'], (day, { isReportSafe, isReportSafeable, part1, part2 }) => {
 	// prettier-ignore
-	cases( day, [
-    [['foo'], 0],
-  ], (input, output) => {
-    strictEqual(part1(input), output);
-  });
+	cases(
+		day,
+		[
+			[[7, 6, 4, 2, 1], true],
+			[[1, 2, 7, 8, 9], false],
+		],
+		(input, output) => {
+			strictEqual(isReportSafe(input), output);
+		},
+	);
 
 	// prettier-ignore
-	cases(day, [
-    [['foo'], 0],
-  ], (input, output) => {
-    strictEqual(part2(input), output);
-  });
+	cases(
+		day,
+		[
+			[[1, 2, 7, 8, 9], false],
+			[[1, 3, 2, 4, 5], true],
+		],
+		(input, output) => {
+			strictEqual(isReportSafeable(input), output);
+		},
+	);
 
 	sample(day, (input) => {
-		strictEqual(part1(input), 0);
-		strictEqual(part2(input), 0);
+		strictEqual(part1(input), 2);
+		strictEqual(part2(input), 4);
 	});
 });
--- 2024/02/02.ts
+++ 2024/02/02.ts
@@ -1,26 +1,52 @@
-function parse(input: string[]): number[] {
-	const data: number[] = [];
+type Report = number[];
+
+function parse(input: string[]): Report[] {
+	const data: Report[] = [];
 
 	for (const line of input) {
-		const a = parseInt(line, 10);
-		data.push(a);
+		const report = line.split(' ').map(Number);
+		data.push(report);
 	}
 
 	return data;
 }
 
-export function part1(input: string[]): number {
-	let result = 0;
+export function isReportSafe(report: Report): boolean {
+	const difference = report.slice(1).map((value, index) => report[index] - value);
 
+	const sign = Math.sign(Math.sign(difference[0]) + Math.sign(difference[difference.length - 1]));
+
+	return difference.every(
+		(value) => Math.abs(value) >= 1 && Math.abs(value) <= 3 && Math.sign(value) === sign,
+	);
+}
+
+export function isReportSafeable(report: Report): boolean {
+	if (isReportSafe(report)) {
+		return true;
+	}
+
+	for (let index = 0; index < report.length; index++) {
+		const newReport = [...report];
+
+		newReport.splice(index, 1);
+
+		if (isReportSafe(newReport)) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+export function part1(input: string[]): number {
 	const data = parse(input);
 
-	return result;
+	return data.map(isReportSafe).filter(Boolean).length;
 }
 
 export function part2(input: string[]): number {
-	let result = 0;
-
 	const data = parse(input);
 
-	return result;
+	return data.map(isReportSafeable).filter(Boolean).length;
 }
