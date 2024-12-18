2c84e6d59f1acc15e2fdbf8de540f66b37ea43e0
Valentin Agachi <github-com@agachi.name>
2024-12-18 18:27:26 +0000

    Coords tests

--- utils/Coords.test.ts
+++ utils/Coords.test.ts
@@ -1,6 +1,7 @@
 import { strictEqual } from 'node:assert';
 import test, { beforeEach, describe } from 'node:test';
 import Coords from './Coords';
+import Matrix from './Matrix';
 
 describe('Coords', () => {
 	var pos: Coords;
@@ -14,6 +15,16 @@ describe('Coords', () => {
 
 		strictEqual(pos.x, 11);
 		strictEqual(pos.y, 7);
+
+		pos.add([0, 1]);
+
+		strictEqual(pos.x, 11);
+		strictEqual(pos.y, 8);
+
+		const newPos = Coords.add(Coords.raw(1, 2), Coords.raw(10, 5));
+
+		strictEqual(newPos.x, 11);
+		strictEqual(newPos.y, 7);
 	});
 
 	test('minus', () => {
@@ -21,6 +32,30 @@ describe('Coords', () => {
 
 		strictEqual(pos.x, -9);
 		strictEqual(pos.y, -3);
+
+		const newPos = Coords.minus(Coords.raw(1, 2), Coords.raw(2, 5));
+
+		strictEqual(newPos.x, -1);
+		strictEqual(newPos.y, -3);
+	});
+
+	test('neighbors', () => {
+		for (const [pos, expected] of [
+			[Coords.raw(0, 0), '0x-1,1x0,0x1,-1x0'],
+			[Coords.raw(1, 2), '1x1,2x2,1x3,0x2'],
+		] as const) {
+			strictEqual(pos.neighbors().join(','), expected);
+		}
+
+		const matrix = Matrix.initialize(3, 3, 0);
+
+		for (const [pos, expected] of [
+			[Coords.raw(0, 0), '1x0,0x1'],
+			[Coords.raw(1, 1), '1x0,2x1,1x2,0x1'],
+			[Coords.raw(2, 2), '2x1,1x2'],
+		] as const) {
+			strictEqual(pos.neighbors(matrix).join(','), expected);
+		}
 	});
 
 	test('negative', () => {
@@ -28,14 +63,26 @@ describe('Coords', () => {
 
 		strictEqual(pos.x, -1);
 		strictEqual(pos.y, -2);
+
+		const newPos = Coords.negative(Coords.raw(1, 2));
+
+		strictEqual(newPos.x, -1);
+		strictEqual(newPos.y, -2);
 	});
 
 	test('wrap', () => {
+		const max = Coords.raw(3, 3);
+		const matrix = Matrix.initialize(3, 3, 0);
+
 		for (const [pos, bounds, expectedX, expectedY] of [
-			[Coords.raw(1, 2), Coords.raw(3, 3), 1, 2],
-			[Coords.raw(3, 3), Coords.raw(3, 3), 0, 0],
-			[Coords.raw(4, 5), Coords.raw(3, 3), 1, 2],
-			[Coords.raw(-1, -2), Coords.raw(3, 3), 2, 1],
+			[Coords.raw(1, 2), max, 1, 2],
+			[Coords.raw(3, 3), max, 0, 0],
+			[Coords.raw(4, 5), max, 1, 2],
+			[Coords.raw(-1, -2), max, 2, 1],
+			[Coords.raw(1, 2), matrix, 1, 2],
+			[Coords.raw(3, 3), matrix, 0, 0],
+			[Coords.raw(4, 5), matrix, 1, 2],
+			[Coords.raw(-1, -2), matrix, 2, 1],
 		] as const) {
 			pos.wrap(bounds);
 
--- utils/Coords.ts
+++ utils/Coords.ts
@@ -1,3 +1,5 @@
+import { ORIENTATIONS, OrientationVector2D } from './constants';
+import Matrix from './Matrix';
 import { Vector2D } from './types';
 
 export default class Coords {
@@ -20,18 +22,29 @@ export default class Coords {
 		return new Coords(x, y);
 	}
 
+	static minus(one: Coords, two: Coords | Vector2D) {
+		if (two instanceof Coords) {
+			return Coords.raw(one.x - two.x, one.y - two.y);
+		}
+		return Coords.raw(one.x - two[0], one.y - two[1]);
+	}
+
+	static negative(item: Coords) {
+		return Coords.raw(-item.x, -item.y);
+	}
+
 	static raw(x: number, y: number) {
 		return new Coords(x, y);
 	}
 
-	addVector2D(vector: Vector2D) {
-		this.x += vector[0];
-		this.y += vector[1];
-	}
-
-	add(other: Coords) {
-		this.x += other.x;
-		this.y += other.y;
+	add(other: Coords | Vector2D) {
+		if (other instanceof Coords) {
+			this.x += other.x;
+			this.y += other.y;
+		} else {
+			this.x += other[0];
+			this.y += other[1];
+		}
 	}
 
 	equal(other: Coords): boolean {
@@ -48,23 +61,32 @@ export default class Coords {
 		this.y = -this.y;
 	}
 
+	neighbors(matrix?: Matrix<unknown>): Coords[] {
+		return ORIENTATIONS.map((orientation) =>
+			Coords.add(this, OrientationVector2D[orientation]),
+		).filter((neighbor) => (matrix ? matrix.inBounds(neighbor) : true));
+	}
+
 	toString() {
 		return `${this.x}x${this.y}`;
 	}
 
-	wrap(bounds: Coords) {
+	wrap(bounds: Matrix<unknown> | Coords) {
+		const maxX = bounds instanceof Matrix ? bounds.sizeX : bounds.x;
+		const maxY = bounds instanceof Matrix ? bounds.sizeY : bounds.y;
+
 		if (this.x < 0) {
-			this.x += bounds.x;
+			this.x += maxX;
 		}
-		if (this.x > bounds.x - 1) {
-			this.x -= bounds.x;
+		if (this.x > maxX - 1) {
+			this.x -= maxX;
 		}
 
 		if (this.y < 0) {
-			this.y += bounds.y;
+			this.y += maxY;
 		}
-		if (this.y > bounds.y - 1) {
-			this.y -= bounds.y;
+		if (this.y > maxY - 1) {
+			this.y -= maxY;
 		}
 	}
 }
