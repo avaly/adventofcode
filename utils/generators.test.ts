2868a78d58c87576b77ea5fae1a212af24fa487c
Valentin Agachi <github-com@agachi.name>
2025-01-26 10:56:47 +0000

    More flexible generators

--- utils/generators.test.ts
+++ utils/generators.test.ts
@@ -5,7 +5,7 @@ import { isGeneratorObject } from 'node:util/types';
 
 describe('generators', () => {
 	test('all combinations', () => {
-		const gen = generateAllCombinations(['a', 'b', 'c', 'd'], 2);
+		const gen = generateAllCombinations(['a', 'b', 'c', 'd'], [2, 2]);
 
 		ok(isGeneratorObject(gen));
 
@@ -24,22 +24,25 @@ describe('generators', () => {
 
 		const result: string[][] = [];
 
-		for (const combo of generateAllCombinations(['a', 'b', 'c'], 2, [])) {
+		for (const combo of generateAllCombinations(['a', 'b', 'c'], [1, 2], [])) {
 			result.push(combo);
 		}
 
 		deepStrictEqual(result, [
+			['a'],
 			['a', 'b'],
 			['a', 'c'],
+			['b'],
 			['b', 'a'],
 			['b', 'c'],
+			['c'],
 			['c', 'a'],
 			['c', 'b'],
 		]);
 	});
 
 	test('unique combinations', () => {
-		const gen = generateUniqueCombinations(['a', 'b', 'c', 'd'], 2);
+		const gen = generateUniqueCombinations(['a', 'b', 'c', 'd'], [2, 2]);
 
 		ok(isGeneratorObject(gen));
 
@@ -52,14 +55,21 @@ describe('generators', () => {
 
 		const result: string[][] = [];
 
-		for (const combo of generateUniqueCombinations(['a', 'b', 'c'], 2, [])) {
+		for (const combo of generateUniqueCombinations(['a', 'b', 'c', 'd'], [2, 3], [])) {
 			result.push(combo);
 		}
 
 		deepStrictEqual(result, [
 			['a', 'b'],
+			['a', 'b', 'c'],
+			['a', 'b', 'd'],
 			['a', 'c'],
+			['a', 'c', 'd'],
+			['a', 'd'],
 			['b', 'c'],
+			['b', 'c', 'd'],
+			['b', 'd'],
+			['c', 'd'],
 		]);
 	});
 });
--- utils/generators.ts
+++ utils/generators.ts
@@ -1,42 +1,43 @@
 export function* generateAllCombinations<T>(
 	items: T[],
-	count: number,
+	counts: [number, number],
 	selected: number[] = [],
 ): IterableIterator<T[]> {
-	if (selected.length === count) {
+	if (selected.length >= counts[0] && selected.length <= counts[1]) {
 		yield selected.map((index) => items[index]);
-		return;
+		if (selected.length === counts[1]) {
+			return;
+		}
 	}
 
 	for (let i = 0; i < items.length; i++) {
 		let valid = !selected.includes(i);
 
 		if (valid) {
-			yield* generateAllCombinations(items, count, [...selected, i]);
+			yield* generateAllCombinations(items, counts, [...selected, i]);
 		}
 	}
 }
 
 export function* generateUniqueCombinations<T>(
 	items: T[],
-	count: number,
+	counts: [number, number],
 	selected: number[] = [],
 ): IterableIterator<T[]> {
-	// console.log('selected', selected, count);
-
-	if (selected.length === count) {
+	if (selected.length >= counts[0] && selected.length <= counts[1]) {
 		yield selected.map((index) => items[index]);
-		return;
+		if (selected.length === counts[1]) {
+			return;
+		}
 	}
 
 	let max = Math.max(...selected, 0);
-	// console.log('max', max);
 
 	for (let i = max; i < items.length; i++) {
 		let valid = !selected.includes(i);
 
 		if (valid) {
-			yield* generateUniqueCombinations(items, count, [...selected, i]);
+			yield* generateUniqueCombinations(items, counts, [...selected, i]);
 		}
 	}
 }
